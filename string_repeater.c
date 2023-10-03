1e47a4661d7bcf076857d6692da6bc5bb4e235f8
zahaale20 <alexcz@outlook.com>
2023-10-03 16:17:48 -0700

    Updated string_repeater.c and deleted a.out

--- string_repeater.c
+++ string_repeater.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 
 int main(int count, char *args[]) { // Parameters: number of arguments, arguments
-   if (count != 3) { // if there isn't three arguments...
+   if (count != 3) { // If there isn't three arguments...
       fprintf(stderr, "Error: Incorrect Argument Usage. Ex: ./string_repeater 5 'Hello World'"); // report an error
-      return 1; // return 1 shows there is an error
+      return 1; // Return 1 shows there is an error
    }
 
    char * endptr;
-   int num = strtol(args[1], &endptr, 10);
+   int num = strtol(args[1], &endptr, 10); // Convert input for count (num times to print string) to an integer
    
-   int i;
-   for(i = 0; i < num; i++){
-      printf("%s\n", args[2]);
-   }
+   if(num >= 0){ // If input 'num' is non negative...
+      // Use a for loop to print the inputted string a 'num' number of times
+      int i;
+      for(i = 0; i < num; i++){
+         printf("%s\n", args[2]);
+      }
 
-   return 0;
+      return 0;
+   } else {
+
+   }
 }
