2d41cfa191101938f344e1d785393e7b562a8121
zahaale20 <alexcz@outlook.com>
2023-10-03 15:50:23 -0700

    Updated string_repeater.c to demonstrate progress.

--- string_repeater.c
+++ string_repeater.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
-void function_one(int input) {
-   int function_one_local = input;
-}
-
-int main(int count, char *args[]) { // Parameters: (number of times to print args, string that doesn't exceed one line)
-   if (count != 3) {
-      fprintf(stderr, "Error: Incorrect Argument Usage. Ex: ./string_repeater 5 'Hello World'");
+int main(int count, char *args[]) { // Parameters: number of arguments, arguments
+   if (count != 3) { // if there isn't three arguments...
+      fprintf(stderr, "Error: Incorrect Argument Usage. Ex: ./string_repeater 5 'Hello World'"); // report an error
+      return 1; // return 1 shows there is an error
    }
 
+   printf("count: %s\n", args[1]);
+   printf("string: %s\n", args[2]);
+
+   char * endptr;
+   int num = strtol(args[2], &endptr, 10);
+   
+   int i;
+   for(i = 0; i < num; i++){
+      printf("%s\n", args[2]);
+   }
+
+   return 0;
 }
